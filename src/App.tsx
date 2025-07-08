eb2162b7ef6b5068973902213513ca4402f74637
Rohit Dhanjee <145933724+RohitDhanjee@users.noreply.github.com>
2025-07-09 03:09:42 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
-import { SettingsProvider } from "@/contexts/SettingsContext";
+import { SettingsProvider } from "@/contexts/Settingscontext";
 import Index from "./pages/Index";
 import Products from "./pages/Products";
 import Product from "./pages/Product";
